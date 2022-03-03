266b99a390338a7494c0475f2801d13ef3a4bf8c
Shameer Iqbal <shameeriqbal@Shameers-MacBook-Pro.local>
2022-03-02 20:14:59 -0400

    finishing iris demo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,22 @@
 import streamlit as st
 from sklearn import tree
+from joblib import load
+from sklearn import datasets
 
-st.Title("SMU model demo")
+
+clf = load("iris.joblib")
+st.title("SMU model demo")
+
+iris = datasets.load_iris()
+
+labels = iris.target_names
+inputs = iris.feature_names
+
+var_1 = st.slider(inputs[0], max_value=10)
+var_2 = st.slider(inputs[1], max_value=10)
+var_3 = st.slider(inputs[2], max_value=10)
+var_4 = st.slider(inputs[3], max_value=10)
+
+prediction = clf.predict([[var_1, var_2, var_3, var_4]])
+
+st.write(labels[prediction[0]])
