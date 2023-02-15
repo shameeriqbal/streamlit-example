8f27676e5bc3c5052899675d335c566dbfee00ee
Shameer Iqbal <shameeriqbal@Shameers-MacBook-Pro.local>
2023-02-15 19:57:49 -0400

    adding new model and code

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import streamlit as st
+from joblib import load
 
-st.title("Test")
+st.title("Deploying the model")
+LABELS = ['setosa', 'versicolor', 'virginica']
+
+clf = load("DT.joblib")
+sp_l = st.slider('sepal length (cm)',
+                 min_value=0,
+                 max_value=10)
+sp_w = st.slider('sepal width (cm)',
+                 min_value=0,
+                 max_value=10)
+pe_l = st.slider('petal length (cm)',
+                 min_value=0,
+                 max_value=10)
+
+pe_w = st.slider('petal width (cm)',
+                 min_value=0,
+                 max_value=10)
+
+prediction = clf.predict([[sp_l, sp_w, pe_l, pe_w]])
+
+st.write(LABELS[prediction])
